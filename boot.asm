ec3a2ebdea608db08936ea9d57a798c627e38b2a
Egor Schelkonogov <egorshelk@gmail.com>
2011-12-15 17:01:32 +0600

    Переделал поиск загрузочного сектора, дизассемблерный код

--- boot.asm
+++ boot.asm
@@ -22,172 +22,210 @@
 ; ===========================================================================
 
 ; Segment type:	Pure code
-seg000		segment	byte public 'CODE' use32
+seg000		segment	byte public 'CODE' use16
 		assume cs:seg000
 		assume es:nothing, ss:nothing, ds:nothing, fs:nothing, gs:nothing
+
+loc_0:
 		cli
-		xor	eax, eax
+		xor	ax, ax
 		mov	ss, ax
-		mov	esp, 0E5897BECh
+		mov	sp, 7BECh
+		mov	bp, sp
 		mov	ds, ax
-		mov	esi, 0C08E7C00h
-		mov	edi, 0B90600h
-		add	esp, edi
-		rep movsd
-		jmp	far ptr	0C030h:61Fh
+		mov	si, 7C00h
+		mov	es, ax
+		mov	di, 600h
+		mov	cx, 100h
+		cld
+		rep movsw
+		jmp	far ptr	0:61Fh
 ; ---------------------------------------------------------------------------
+		xor	al, al
 		cmp	dl, al
 		jnz	short loc_27
 		mov	dl, 80h	; '€'
 
-loc_27:					; CODE XREF: seg000:00000023j
-		mov	[esi+12h], dl
-		mov	esi, 20BF0790h
-		pop	es
-		call	edi
-		mov	esi, 4B907BEh
-		add	[eax+eax*4+5C8AC030h], dh
-		add	[eax], bh
-		fdiv	qword ptr [ebx+edx+38h]
-		fdiv	dword ptr [ebp+1Fh]
-		add	esi, 75490010h
-		out	dx, al
-		mov	esi, 0FDBF0733h
-		push	es
-		call	edi
-		mov	edi, esi
+loc_27:					; CODE XREF: seg000:0023j
+		mov	[bp+12h], dl
+		mov	si, 790h
+		mov	di, 720h
+		call	di
+		mov	si, 7BEh
+		mov	cx, 4
+		mov	ah, 80h	; '€'
+		xor	al, al
 
-loc_58:					; CODE XREF: seg000:00000064j
-		add	esi, 74490010h
-		jp	near ptr 0DC3800C0h
+loc_3C:					; CODE XREF: seg000:loc_4Cj
+		mov	bl, [si+0]
+		cmp	ah, bl		; DATA XREF: seg000:012Ar
+		jz	short loc_56
+		cmp	al, bl
+		jnz	short loc_66
+		add	si, 10h
+		dec	cx
+
+loc_4C:					; DATA XREF: seg000:0076r seg000:00BEr ...
+		jnz	short loc_3C
+		mov	si, 733h
+		mov	di, 6FDh
+		call	di
+
+loc_56:					; CODE XREF: seg000:0041j
+		mov	di, si
+
+loc_58:					; CODE XREF: seg000:0064j
+					; DATA XREF: seg000:010Cr
+		add	si, 10h
+		dec	cx
+		jz	short loc_6E
+		mov	bl, [si+0]
+		cmp	ah, bl
 		jnz	short loc_58
-		mov	esi, 0FDBF074Ah
-		push	es
-		call	edi
-		mov	ah, 41h	; 'A'
-		mov	ebx, 568A55AAh
-		adc	cl, ch
-		adc	esi, [edx+50h]
-		cmp	ebx, 4A75AA55h
-		and	ecx, 44740001h
-		mov	byte ptr [esi+0], 10h
-		mov	byte ptr [esi+1], 0
-		mov	byte ptr [esi+2], 1
-		mov	byte ptr [esi+3], 0
-		mov	dword ptr [esi+4], 46C77C00h
-		push	es
-; ---------------------------------------------------------------------------
-		db    0
-; ---------------------------------------------------------------------------
-		add	[ebx-76FFF77Bh], cl
-		xchg	cl, [eax]
 
-loc_A7:					; CODE XREF: seg000:000000D9j
-		add	[ebx-76FFF57Bh], cl
-		xchg	cl, [edx]
-		add	bh, al
-		inc	esi
-		or	al, 0
-		add	bh, al
-		inc	esi
-		push	cs
+loc_66:					; CODE XREF: seg000:0045j
+		mov	si, 74Ah
+		mov	di, 6FDh
+		call	di
+
+loc_6E:					; CODE XREF: seg000:005Dj
+		mov	ah, 41h	; 'A'
+		mov	bx, 55AAh
+		mov	dl, [bp+12h]
+		int	13h		; DISK -
+		jb	short loc_CA
+		cmp	bx, 0AA55h
+		jnz	short loc_CA
+		and	cx, 1
+		jz	short loc_CA
+		mov	byte ptr [bp+0], 10h
+		mov	byte ptr [bp+1], 0
+		mov	byte ptr [bp+2], 1
+		mov	byte ptr [bp+3], 0
+		mov	word ptr [bp+4], 7C00h
+		mov	word ptr [bp+6], 0
+		mov	ax, [di+8]
+		mov	[bp+8],	ax
+		mov	ax, [di+0Ah]
+		mov	[bp+0Ah], ax
+		mov	word ptr [bp+0Ch], 0
+		mov	word ptr [bp+0Eh], 0
+		mov	si, bp
+		mov	ah, 42h	; 'B'
+		int	13h		; DISK -
+		jnb	short loc_E6
+		mov	si, 756h
+		mov	di, 6FDh
+		call	di
+
+loc_CA:					; CODE XREF: seg000:0078j seg000:007Ej ...
+		mov	dh, [di+1]
+		mov	cx, [di+2]
+		mov	ax, 201h
+		mov	dl, [bp+12h]
+		mov	bx, 7C00h
+		int	13h		; DISK - READ SECTORS INTO MEMORY
+					; AL = number of sectors to read, CH = track, CL = sector
+					; DH = head, DL	= drive, ES:BX -> buffer to fill
+					; Return: CF set on error, AH =	status,	AL = number of sectors read
+		jnb	short loc_E6
+		mov	si, 756h
+		mov	di, 6FDh
+		call	di
+
+loc_E6:					; CODE XREF: seg000:00C0j seg000:00DCj
+		mov	bx, 7C00h
+		mov	ax, [bx+1FEh]
+		cmp	ax, 0AA55h
+		jnz	short loc_FA
+		mov	si, di
+		jmp	far ptr	0:7C00h
 ; ---------------------------------------------------------------------------
-		db    0
-; ---------------------------------------------------------------------------
-		add	[ecx-32BD4B12h], cl
-		adc	esi, [ebx+24h]
-		mov	esi, 0FDBF0756h
-		push	es
-		call	edi
-		mov	dh, [ebp+1]
-		mov	ecx, [ebp+1B80002h]
-		add	cl, [edx+0BB1256h]
-		jl	short near ptr loc_A7+1
-		adc	esi, [ebx+8]
-		mov	esi, 0FDBF0756h
-		push	es
-		call	edi
-		mov	ebx, 878B7C00h
-		inc	byte ptr [ecx]
-		cmp	eax, 775AA55h
-		mov	esi, edi
-		jmp	far ptr	60BEh:7C00h
-; ---------------------------------------------------------------------------
-		pop	es
-		mov	edi, 0D7FF0720h
-		mov	esi, 20BF076Ch
-		pop	es
-		call	edi
-		xor	eax, eax
+
+loc_FA:					; CODE XREF: seg000:00F1j
+		mov	si, 760h
+		mov	di, 720h
+		call	di
+		mov	si, 76Ch
+		mov	di, 720h
+		call	di
+		xor	ax, ax
 		int	16h		; KEYBOARD - READ CHAR FROM BUFFER, WAIT IF EMPTY
 					; Return: AH = scan code, AL = character
-		xor	ebx, ebx
+		xor	bx, bx
 		mov	es, bx
-		mov	ebx, 34B80472h
-		adc	ah, [esi]
-		mov	[edi], eax
-		jmp	far ptr	3160h:0FFFF0000h
+		mov	bx, 472h
+		mov	ax, 1234h
+		mov	es:[bx], ax
+		jmp	far ptr	0FFFFh:0
 ; ---------------------------------------------------------------------------
-
-loc_122:				; CODE XREF: seg000:0000012Fj
-		fld	tbyte ptr [eax+ecx-4BF68B40h]
-		push	cs
-		int	10h		; - VIDEO -
+		pusha
+		xor	bx, bx
 		lodsb
 		or	al, al
-		jnz	short near ptr loc_122+6
+		jz	short loc_131
+
+loc_128:				; CODE XREF: seg000:012Fj
+		mov	ah, 0Eh
+		int	10h		; - VIDEO - WRITE CHARACTER AND	ADVANCE	CURSOR (TTY WRITE)
+					; AL = character, BH = display page (alpha modes)
+					; BL = foreground color	(graphics modes)
+		lodsb
+		or	al, al
+		jnz	short loc_128
+
+loc_131:				; CODE XREF: seg000:0126j
 		popa
 		retn
 ; ---------------------------------------------------------------------------
-		dec	esi
-		outsd
-		and	[ecx+63h], ah
+		dec	si
+		outsw
+		and	[bx+di+63h], ah
 		jz	short near ptr unk_1A3
 		jbe	short near ptr unk_1A1
-		and	[ebp+6Eh], ah
+		and	[di+6Eh], ah
 		jz	short near ptr unk_1B3
 		jns	short near ptr loc_162+1
-; ---------------------------------------------------------------------------
-		db  69h	; i
-; ---------------------------------------------------------------------------
-		outsb
-		and	[ebp+42h], cl
-		push	edx
-		add	[ecx+6Eh], cl
+		imul	bp, [bp+20h], 424Dh
+		push	dx
+		add	[bx+di+6Eh], cl
 		jbe	short near ptr unk_1AF
 		insb
-		imul	esp, [eax+4Dh],	49005242h
-		das
-		dec	edi
-		and	[ebp+72h], ah
+		imul	sp, [si+20h], 424Dh
+		push	dx
+		add	[bx+di+2Fh], cl
+		dec	di
+		and	[di+72h], ah
 		jb	short near ptr unk_1CD
 		jb	short $+2
-		dec	ecx
+		dec	cx
 		outsb
 
-loc_162:				; CODE XREF: seg000:00000141j
-		jbe	short loc_1C5
+loc_162:				; CODE XREF: seg000:0141j
+		jbe	short near ptr loc_1C4+1
 		insb
-		imul	esp, [eax+50h],	0D005342h
-		or	dl, [eax+72h]
+		imul	sp, [si+20h], 4250h
+		push	bx
+		add	[di], cl
+		or	dl, [bx+si+72h]
 		db	65h
 		jnb	short near ptr unk_1E6
-		and	[ecx+6Ch], ah
-		insd
-		outsd
+		and	[bx+di+6Ch], ah
+		insw
+		outsw
 		jnb	short near ptr unk_1EE
-		and	[ecx+6Eh], ah
+		and	[bx+di+6Eh], ah
 		jns	short near ptr unk_19F
-		imul	esp, [ebp+79h],	20h
+		imul	sp, [di+79h], 20h
 		jz	short near ptr unk_1F4
-		and	[edx+65h], dh
-		bound	ebp, [edi+6Fh]
+		and	[bp+si+65h], dh
+		bound	bp, [bx+6Fh]
 		jz	short near ptr unk_1BB
 		db	2Eh
-		add	cs:[ebp+42h], cl
-		push	edx
+		add	cs:[di+42h], cl
+		push	dx
 		db	2Eh, 2Eh
-		add	cs:[eax], al
+		add	cs:[bx+si], al
 ; ---------------------------------------------------------------------------
 		db    0
 		db    0
@@ -196,13 +234,11 @@ loc_162:				; CODE XREF: seg000:00000141j
 		db    0
 		db    0
 		db    0
-unk_19F		db    0			; CODE XREF: seg000:0000017Dj
+unk_19F		db    0			; CODE XREF: seg000:017Dj
 		db    0
-unk_1A1		db    0			; CODE XREF: seg000:0000013Aj
+unk_1A1		db    0			; CODE XREF: seg000:013Aj
 		db    0
-unk_1A3		db    0			; CODE XREF: seg000:00000138j
-		db    0
-		db    0
+unk_1A3		db    0			; CODE XREF: seg000:0138j
 		db    0
 		db    0
 		db    0
@@ -212,11 +248,13 @@ unk_1A3		db    0			; CODE XREF: seg000:00000138j
 		db    0
 		db    0
 		db    0
-unk_1AF		db    0			; CODE XREF: seg000:0000014Cj
+		db    0
+		db    0
+unk_1AF		db    0			; CODE XREF: seg000:014Cj
 		db    0
 		db    0
 		db    0
-unk_1B3		db    0			; CODE XREF: seg000:0000013Fj
+unk_1B3		db    0			; CODE XREF: seg000:013Fj
 		db    0
 		db    0
 		db    0
@@ -224,32 +262,25 @@ unk_1B3		db    0			; CODE XREF: seg000:0000013Fj
 		db    0
 		db    0
 		db    0
-unk_1BB		db    0			; CODE XREF: seg000:0000018Bj
+unk_1BB		db    0			; CODE XREF: seg000:018Bj
 		db    0
 		db    0
-		db  80h	; €
-		db    1
-		db    1
-		db    0
-		db  39h	; 9
-		db  3Fh	; ?
-		db 0BFh	; ¿
 ; ---------------------------------------------------------------------------
-
-loc_1C5:				; CODE XREF: seg000:loc_162j
-		pop	es
+		add	byte ptr [bx+di], 1
+		add	[bx+di], bh
 		aas
+
+loc_1C4:				; CODE XREF: seg000:loc_162j
+		mov	di, 3F07h
 ; ---------------------------------------------------------------------------
 		db    0
 		db    0
-		db    0
-		db 0C1h
-		db 0FDh	; ý
-		db  1Fh
-unk_1CD		db    0			; CODE XREF: seg000:0000015Cj
-		db    0
-		db    0
-		db    0
+; ---------------------------------------------------------------------------
+		add	cl, al
+		std
+		pop	ds
+; ---------------------------------------------------------------------------
+unk_1CD		db    0			; CODE XREF: seg000:015Cj
 		db    0
 		db    0
 		db    0
@@ -271,7 +302,10 @@ unk_1CD		db    0			; CODE XREF: seg000:0000015Cj
 		db    0
 		db    0
 		db    0
-unk_1E6		db    0			; CODE XREF: seg000:00000170j
+		db    0
+		db    0
+		db    0
+unk_1E6		db    0			; CODE XREF: seg000:0170j
 		db    0
 		db    0
 		db    0
@@ -279,13 +313,13 @@ unk_1E6		db    0			; CODE XREF: seg000:00000170j
 		db    0
 		db    0
 		db    0
-unk_1EE		db    0			; CODE XREF: seg000:00000178j
+unk_1EE		db    0			; CODE XREF: seg000:0178j
 		db    0
 		db    0
 		db    0
 		db    0
 		db    0
-unk_1F4		db    0			; CODE XREF: seg000:00000183j
+unk_1F4		db    0			; CODE XREF: seg000:0183j
 		db    0
 		db    0
 		db    0
@@ -301,3 +335,4 @@ seg000		ends
 
 
 		end
+
--- findBoot.cpp
+++ findBoot.cpp
@@ -1,19 +1,43 @@
 #include <cstdio>
+#include <vector>
+using namespace std;                                                     
+void printBoot(FILE *in, long off)
+{
+	fseek(in, off - 512, SEEK_SET);
+	char fname[20];
+	sprintf(fname, "boot%ld.bin", off);
+	FILE *out = fopen(fname, "wb");
+
+	for (int i = 0; i < 512; ++i)
+	{
+		char tmp = getc(in);
+		putc(tmp, out);
+	}
+	fclose(out);
+}
 int main()
 {
 	FILE *in = fopen("plan9", "rb");
-	FILE *out = fopen("boot.out", "wb");
-	char prv = getc(in);
-	putc(prv, out);
-	int cnt = 1;
+
+	char prv = getc(in);	
+	long pos = 1;
+	vector<long> positions;
 	while(!feof(in))
 	{
 		char cur = getc(in);
-		putc(cur, out);
-		++cnt;
-		if (prv == char(0x55) && cur == char(0xAA) && cnt >= 512)
-			break;
+		++pos;
+
+		if (prv == char(0x55) && cur == char(0xAA) && pos >= 512)
+		{
+		    positions.push_back(pos);
+		    printf("%ld\n", pos);
+		}
 		prv = cur;
 	}
+	for(vector<long>::iterator it = positions.begin(); it != positions.end(); ++it)
+	{
+		printBoot(in, *it);
+	}
+	fclose(in);
 	return 0;
 }
