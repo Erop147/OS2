8efd1a465ea83b0d6fad013425ec496f828c808f
Egor Schelkonogov <egorshelk@gmail.com>
2011-12-15 16:10:20 +0600

    исправил первое задание

--- first.cpp
+++ first.cpp
@@ -7,40 +7,47 @@ void add(char *name, vector<int> &res)
 	FILE *in = fopen(name, "r");
 	if (in == NULL)
 	{
-		fprintf(stderr, "file %s dosn't exist\n", name);
+		fprintf(stderr, "can't open file %s\n", name);
 		return;
 	}
 	int num;
-    while (!feof(in))
+	try
     {
-    	if (fscanf (in, "%d", &num) <= 0)
-    	{
-    		fprintf(stderr, "file %s contains non digit\n", name);
-    		break;
-    	}
-    	try
-    	{
+	    while (fscanf (in, "%d", &num) > 0)
+    	{	
     		res.push_back(num);
+	    }
+		if (!feof(in))
+	   	{
+    		fprintf(stderr, "file %s contains non digit\n", name);
     	}
-    	catch (exception e)
-    	{
-    		fprintf(stderr, "something wrong with vectors");
-    		break;
-    	}
-    }
-	fclose(in);
+    
+   	}
+   	catch (exception e)
+   	{
+   		fprintf(stderr, "something wrong with vectors\n");
+   	}
+   	if (fclose(in))
+   	{
+   		fprintf(stderr, "can't close file %s \n", name);
+   	}
 }
 int main(int argv, char* args[])
 {
 	vector<int> numbers;
+	if (argv < 2)
+	{
+		fprintf(stderr, "Too few arguments");
+		return 0;
+	}
 	for (int i = 1; i < argv; ++i)                       
 	{
 		add(args[i], numbers);
 	}
 	sort(numbers.begin(), numbers.end());
-	for (int i = 0; i < (int)numbers.size(); ++i)
+	for (vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it)
 	{
-		printf ("%d ", numbers[i]);
+		printf ("%d ", *it);
 	}
 	return 0; 
 }
