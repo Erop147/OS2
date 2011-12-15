5a150abf1255f5f474bc10fc484368f0927ac739
Egor Schelkonogov <egorshelk@gmail.com>
2011-12-15 17:50:58 +0600

    Проверка на ошибки

--- findBoot.cpp
+++ findBoot.cpp
@@ -1,23 +1,36 @@
 #include <cstdio>
 #include <vector>
-using namespace std;                                                     
+using namespace std;
+
+const char imageName[] = "plan9";
+                                                     
 void printBoot(FILE *in, long off)
 {
 	fseek(in, off - 512, SEEK_SET);
-	char fname[20];
-	sprintf(fname, "boot%ld.bin", off);
+	char fname[50];
+	sprintf(fname, "boot%09ld.bin", off);
 	FILE *out = fopen(fname, "wb");
 
 	for (int i = 0; i < 512; ++i)
 	{
 		char tmp = getc(in);
+		if (ferror(in))
+		{
+			printf("Can't read from %s!\n", imageName);
+			break;                           
+		}
 		putc(tmp, out);
+		if (ferror(out))
+		{
+			printf("Can't write to %s!\n", fname);
+			break;                           
+		}
 	}
 	fclose(out);
 }
 int main()
 {
-	FILE *in = fopen("plan9", "rb");
+	FILE *in = fopen(imageName, "rb");
 
 	char prv = getc(in);	
 	long pos = 1;
@@ -25,12 +38,17 @@ int main()
 	while(!feof(in))
 	{
 		char cur = getc(in);
+		if (ferror(in))
+		{
+			printf("Can't read from %s!\n", imageName);
+			break;
+		}
 		++pos;
 
 		if (prv == char(0x55) && cur == char(0xAA) && pos >= 512)
 		{
-		    positions.push_back(pos);
-		    printf("%ld\n", pos);
+			positions.push_back(pos);
+			printf("%ld\n", pos);
 		}
 		prv = cur;
 	}
